2fe3a467059ad741694e781885c51a6a4cb2d349
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-25 16:09:18 -0400

    import FFPA component

--- mmic_ffpa_gmx/__init__.py
+++ mmic_ffpa_gmx/__init__.py
@@ -5,6 +5,10 @@ MMIC for force field parameter association with GMX
 
 # Add imports here
 from .mmic_ffpa_gmx import *
+from .models import *
+from .components import *
+from . import models
+from . import components
 
 # Handle versioneer
 from ._version import get_versions
@@ -13,3 +17,6 @@ versions = get_versions()
 __version__ = versions["version"]
 __git_revision__ = versions["full-revisionid"]
 del get_versions, versions
+
+# Main component for running FFPA
+runComponent = components.ffpa_component.AssignGmxComponent
