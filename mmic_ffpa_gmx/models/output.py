54eb5c9ddb33b59f907be536fc21a4ed91284a67
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-04-08 11:56:32 -0400

    make Field proc_input required

--- mmic_ffpa_gmx/models/output.py
+++ mmic_ffpa_gmx/models/output.py
@@ -1,14 +1,13 @@
 from mmelemental.models import ProcOutput
 from mmic_ffpa.models import AssignInput
 from pydantic import Field
-from typing import Optional
 
 __all__ = ["ComputeGmxOutput"]
 
 
 class ComputeGmxOutput(ProcOutput):
     proc_input: AssignInput = Field(
-        None, description="Procedure input schema."
-    )  # must become required field, eventually
+        ..., description="Procedure input schema."
+    )
     forcefield: str = Field(..., description="Force field params file string object.")
     molecule: str = Field(..., description="Molecule file string object.")
