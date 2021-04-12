8ac82079b5ce3124ee7d3bdb48b4cd071784c547
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-04-12 12:09:27 -0400

    update format with black

--- mmic_ffpa_gmx/models/output.py
+++ mmic_ffpa_gmx/models/output.py
@@ -6,8 +6,6 @@ __all__ = ["ComputeGmxOutput"]
 
 
 class ComputeGmxOutput(ProcOutput):
-    proc_input: AssignInput = Field(
-        ..., description="Procedure input schema."
-    )
+    proc_input: AssignInput = Field(..., description="Procedure input schema.")
     forcefield: str = Field(..., description="Force field params file string object.")
     molecule: str = Field(..., description="Molecule file string object.")
