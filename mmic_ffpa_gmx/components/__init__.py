2a789ef7796c928cafd0a8e3a1a35a768a4c0a1f
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-05-06 13:39:56 -0400

    import only AssignGmxComponent

--- mmic_ffpa_gmx/components/__init__.py
+++ mmic_ffpa_gmx/components/__init__.py
@@ -3,14 +3,4 @@ from . import compute_component
 from . import post_component
 from . import ffpa_component
 
-from .prep_component import *
-from .compute_component import *
-from .post_component import *
-from .ffpa_component import *
-
-__all__ = (
-    prep_component.__all__
-    + compute_component.__all__
-    + post_component.__all__
-    + ffpa_component.__all__
-)
+from .ffpa_component import AssignGmxComponent
