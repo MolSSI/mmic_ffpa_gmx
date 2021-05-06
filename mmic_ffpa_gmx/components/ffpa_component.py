702280e4b2d9bc3cdde262010e5b206a33c33ddb
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-05-06 13:40:59 -0400

    replace SpecificComponent with TacticComponent

--- mmic_ffpa_gmx/components/ffpa_component.py
+++ mmic_ffpa_gmx/components/ffpa_component.py
@@ -6,14 +6,14 @@ from .prep_component import PrepGmxComponent
 from .compute_component import ComputeGmxComponent
 from .post_component import PostGmxComponent
 
-from mmic.components.blueprints import SpecificComponent
-from typing import Optional, Tuple, List
+from mmic.components.blueprints import TacticComponent
+from typing import Optional, Tuple, List, Any
 
 __all__ = ["AssignGmxComponent"]
 
 
-class AssignGmxComponent(SpecificComponent):
-    """ Main entry component for running FF assignment. """
+class AssignGmxComponent(TacticComponent):
+    """Main entry component for running FF assignment."""
 
     @classmethod
     def input(cls):
@@ -36,3 +36,21 @@ class AssignGmxComponent(SpecificComponent):
         computeOutput = ComputeGmxComponent.compute(computeInput)
         assignOutput = PostGmxComponent.compute(computeOutput)
         return True, assignOutput
+
+    def get_version(cls) -> str:
+        """Finds program, extracts version, returns normalized version string.
+        Returns
+        -------
+        str
+            Return a valid, safe python version string.
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def strategy_comp(cls) -> Any:
+        """Returns the strategy component this (tactic) component belongs to.
+        Returns
+        -------
+        Any
+        """
+        return set(mmic_ffpa.components.AssignComponent)
