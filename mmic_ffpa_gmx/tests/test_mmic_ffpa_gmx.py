e88d8d2722625aa695595421cac4514da94c98fa
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-05-06 13:43:57 -0400

    update import paths and test routine

--- mmic_ffpa_gmx/tests/test_mmic_ffpa_gmx.py
+++ mmic_ffpa_gmx/tests/test_mmic_ffpa_gmx.py
@@ -3,12 +3,11 @@ Unit and regression test for the mmic_ffpa_gmx package.
 """
 
 # Import package, test suite, and other packages as needed
-from mmic_ffpa_gmx.components import (
-    PrepGmxComponent,
-    ComputeGmxComponent,
-    PostGmxComponent,
-    AssignGmxComponent,
-)
+from mmic_ffpa_gmx.components import AssignGmxComponent
+from mmic_ffpa_gmx.components.prep_component import PrepGmxComponent
+from mmic_ffpa_gmx.components.post_component import PostGmxComponent
+from mmic_ffpa_gmx.components.compute_component import ComputeGmxComponent
+
 from mmic_ffpa.models import AssignInput
 from mmelemental.models import Molecule
 import mm_data
@@ -28,12 +27,16 @@ def test_mmic_ffpa_sub():
     mfile = mm_data.mols["dialanine.pdb"]
     mol = Molecule.from_file(mfile)
     inputs = AssignInput(
-        molecule={"mol": mol}, forcefield={"mol": "amber99"}, engine="gmx"
+        molecule={"mol": mol},
+        forcefield={"mol": "amber99"},
+        keywords={"-ignh": ...},
+        engine="gmx",
     )
     computeInput = PrepGmxComponent.compute(inputs)
     computeOutput = ComputeGmxComponent.compute(computeInput)
     outp = PostGmxComponent.compute(computeOutput)
     mol, ff = outp.molecule, outp.forcefield
+
 
 @pytest.mark.parametrize("mol_file", ["alanine.gro", "1dzl_fixed.gro"])
 def test_mmic_ffpa_main(mol_file):
@@ -43,7 +46,7 @@ def test_mmic_ffpa_main(mol_file):
         molecule={"mol": mol},
         forcefield={"mol": "amber99"},
         engine="gmx",
-        kwargs={"-ignh": ""},
+        keywords={"-ignh": ...},
     )
     outp = AssignGmxComponent.compute(inputs)
     mol, ff = outp.molecule, outp.forcefield
